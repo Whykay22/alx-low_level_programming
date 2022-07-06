6a3132e0ddc6e0c31f4d97c5f15b17464096f0a2
Yinka Famakinwa <yinka.makinwa@gmail.com>
2022-07-06 11:55:31 +0100

    0. putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Application Entry Point
+ * Return: int
+ */
+int main(void)
+{
+char output[9] = "_putchar\n";
+int i = 0;
+
+while (i < 9)
+{
+_putchar(output[i]);
+i++;
+}
+
+return (0);
+}
