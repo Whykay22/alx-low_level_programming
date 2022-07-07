bda4a57a919cc52f45cd41083d91562e2e0feb61
Yinka Famakinwa <yinka.makinwa@gmail.com>
2022-07-07 01:17:18 +0100

    11 - 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - print from n to 98
+ * @n : number to start printing from
+ * Return: void
+ */
+
+void print_to_98(int n)
+{
+int i = n;
+
+while (i != 98)
+{
+printf("%d, ", i);
+if (i > 98)
+--i;
+else
+++i;
+}
+printf("98\n");
+}
